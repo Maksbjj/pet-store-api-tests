4b016536abb00d08200b844401fa75c8735b8079
Maksim Arsianiuk <m.arsianiuk@zoovu.com>
2023-08-18 15:32:57 +0200

    first test

---
+++ tests/example.spec.ts
@@ -0,0 +1,26 @@
+import { test, expect } from "@playwright/test";
+
+test("has title", async ({ page, request }) => {
+  const response = await request.post("localhost:8080/api/v3/pet", {
+    data: {
+      id: 10,
+      name: "doggie",
+      category: {
+        id: 1,
+        name: "Dogs",
+      },
+      photoUrls: ["string"],
+      tags: [
+        {
+          id: 0,
+          name: "string",
+        },
+      ],
+      status: "available",
+    },
+  });
+
+  // Expect a title "to contain" a substring.
+  await expect(response).toBeOK();
+  console.log(await response.json());
+});
