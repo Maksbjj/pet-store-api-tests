0075cd9e86acc63eee1debe5ee77a3ee201eea8e
Maksim Arsianiuk <m.arsianiuk@zoovu.com>
2023-08-20 20:55:45 +0200

    fix

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,7 +10,7 @@ import { defineConfig, devices } from "@playwright/test";
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: "./tests",
+  testDir: "./src/tests",
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -33,8 +33,7 @@ export default defineConfig({
   /* Configure projects for major browsers */
   projects: [
     {
-      name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
+      name: "users-api",
     },
 
     /* Test against mobile viewports. */
