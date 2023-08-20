64b27321d79ec71c21eb5fdeddff41a4e4c9a9af
Maksim Arsianiuk <m.arsianiuk@zoovu.com>
2023-08-20 21:16:43 +0200

    fix

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,27 +1,17 @@
 import { defineConfig, devices } from "@playwright/test";
+import * as dotenv from "dotenv";
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
+dotenv.config({ path: "./src/setup/.env.variables", override: true });
 
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
   testDir: "./src/tests",
-  /* Run tests in files in parallel */
   fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  reporter: process.env.CI ? [["html"], ["list"], ["blob"]] : [["list"]],
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: process.env.API_URL,
